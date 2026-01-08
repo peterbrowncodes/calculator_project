6df00faec985eea5ea5aa2d1f664c1b59bc45684
peterbrowncodes <99191823+peterbrowncodes@users.noreply.github.com>
2026-01-08 18:22:25 -0500

    initial JS functions

---
+++ script.js
@@ -0,0 +1,27 @@
+function addition(a, b) {
+  const first = a;
+  const second = b;
+  const total = a + b;
+  return total;
+}
+
+function subtract(a, b) {
+  const first = a;
+  const second = b;
+  const total = a - b;
+  return total;
+}
+
+function multiply(a, b) {
+  const first = a;
+  const second = b;
+  const total = a * b;
+  return total;
+}
+
+function divide(a, b) {
+  const first = a;
+  const second = b;
+  const total = a / b;
+  return total;
+}
