e69a171943af85f2618c42c7d892988031a44ed7
peterbrowncodes <99191823+peterbrowncodes@users.noreply.github.com>
2026-01-08 18:38:01 -0500

    creates operate function

--- script.js
+++ script.js
@@ -6,14 +6,26 @@ function addition(a, b) {
   return a + b;
 }
 
-function subtract(a, b) {
+function subtraction(a, b) {
   return a - b;
 }
 
-function multiply(a, b) {
+function multiplication(a, b) {
   return a * b;
 }
 
-function divide(a, b) {
+function division(a, b) {
   return a / b;
+}
+
+function operate(operator, num1, num2) {
+  if (operator === "+") {
+    return addition(num1, num2);
+  } else if (operator === "-") {
+    return subtraction(num1, num2);
+  } else if (operator === "*") {
+    return multiplication(num1, num2);
+  } else if (operator === "/") {
+    return division(num1, num2);
+  };
 }
