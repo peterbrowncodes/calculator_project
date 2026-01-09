480987a8a9e6d5f4243adfada3e00e716d8acac4
peterbrowncodes <99191823+peterbrowncodes@users.noreply.github.com>
2026-01-08 21:16:32 -0500

    added pseudo code and click functionality

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const tds = document.querySelectorAll("td");
 const calcBody = document.getElementById("calc-body");
 const numberTable = document.getElementById("calc-numbers");
 const operatorTable = document.getElementById("calc-operators");
+const calcResult = document.getElementById("result");
+const calcClear = document.getElementById("clear");
+const calcEquals = document.getElementById("equals");
 const calcNumbers = document.querySelectorAll("number");
 const calcOperators = document.querySelectorAll("operator");
-let firstNumber;
+let firstNumber = null;
 let secondNumber;
 let operator;
+let result;
+
 
 function addition(a, b) {
   return a + b;
@@ -24,7 +29,7 @@ function division(a, b) {
   return a / b;
 }
 
-function operate(operator, num1, num2) {
+function operate(num1, operator, num2) {
   if (operator === "+") {
     return addition(num1, num2);
   } else if (operator === "-") {
@@ -41,8 +46,47 @@ function getClass(element) {
 }
 
 calcBody.addEventListener('click', function(event) {
+
   elementText = event.target.textContent;
   elementClass = event.target.className;
+  elementId = event.target.id;
+
+  // check if class of click is number or operator
+  if (elementClass === "number") {
+  // if number
+    // check if first number is null
+      // assign text content to 1st var
+    // if not null 
+      // assign text content to 2nd var
+    if (firstNumber === null) {
+      firstNumber = elementText;
+      console.log("first number: " + firstNumber);
+    } else {
+      secondNumber = elementText;
+      console.log("second number: " + secondNumber);
+    }
+  } else if (elementClass === "operator") {
+  // if operator
+    // assign value to operator var
+    operator = elementText;
+    console.log("operator: " + operator);
+  };
+
+  // clear valriables 
+  if (elementId === "clear") {
+    firstNumber = null;
+    secondNumber = null;
+    operator = null;
+    console.log("cleared vars: " + firstNumber + secondNumber + operator);
+  }
+  // start operation
+
+  // results output
+
   console.log(elementText);
   console.log('Class: ' + elementClass);
+  calcResult.textContent = elementText;
 })
+
+/* RESULT OUTPUT */
+
