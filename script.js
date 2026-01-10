cff878db5281ed99df96021ac1d8aa719126022c
peterbrowncodes <99191823+peterbrowncodes@users.noreply.github.com>
2026-01-10 18:15:43 -0500

    reworked output format and decimal places

--- script.js
+++ script.js
@@ -35,14 +35,21 @@ function operate(num1, num2, operator) {
   if (typeof(num1) === "number" && typeof(num2) === "number") {
     if (operator === "+") {
         return addition(num1, num2);
-      } else if (operator === "-") {
+      } 
+      else if (operator === "-") {
         return subtraction(num1, num2);
-      } else if (operator === "*") {
+      } 
+      else if (operator === "*") {
         return multiplication(num1, num2).toFixed(2);
-      } else if (operator === "/") {
+      } 
+      else if (operator === "/") {
+
+        // if dividing by 0
         if (num1 === 0 || num2 === 0) {
           return 'STOP IT! NOW!';
-        } else {
+        } 
+        
+        else {
           let divisionResult;
           quotient = division(num1, num2);
           if (quotient % 1 !== 0) {
@@ -68,6 +75,15 @@ function joinNum(array) {
   return array.join('');
 }
 
+function clearVars() {
+  firstNumberArray = [];
+  firstNumber = 0;
+  secondNumberArray = [];
+  secondNumber = 0;
+  operator = null;
+  console.log("cleared vars: " + firstNumber + ' ' + secondNumber + ' '+ operator);
+}
+
 calcBody.addEventListener('click', function(event) {
   elementText = event.target.textContent;
   elementClass = event.target.className;
@@ -75,7 +91,8 @@ calcBody.addEventListener('click', function(event) {
 
   // check if class of click is number or operator
   if (elementClass === "number") {
-  // if number or operator
+  // if number
+    // first number
     if (operator === null) {
       selectedNum = Number(elementText);
       concatNum(selectedNum, firstNumberArray);
@@ -84,6 +101,8 @@ calcBody.addEventListener('click', function(event) {
       console.log("first number: " + firstNumber);
       calcResult.textContent = `${firstNumber}`;
     } 
+    
+    // second number
     else {
       selectedNum = Number(elementText);
       concatNum(selectedNum, secondNumberArray);
@@ -92,24 +111,28 @@ calcBody.addEventListener('click', function(event) {
       console.log("second number: " + secondNumber);
       calcResult.textContent = `${firstNumber} ${operator} ${secondNumber}`
     }
+
+  // if operator
   } else if (elementClass === "operator") {
+      operator = elementText;
+      
+      // is operator null
       if (operator === null) {
-        operator = elementText;
         console.log("operator: " + operator);
         calcResult.textContent = `${firstNumber} ${operator}`
       } 
+
+      // operator is not null
       else {
         result = operate(firstNumber, secondNumber, operator);
         console.log(result);
         calcResult.textContent = result;
-        firstNumber = result;
-        secondNumber = 0;
-        operator = null;
-        operator = elementText;
+        firstNumber = Number(result);
         console.log("operator: " + operator);
         calcResult.textContent = `${firstNumber} ${operator}`
         secondNumberArray = [];
         secondNumber = null;
+        operator = null;
       }
 
   };
@@ -117,29 +140,27 @@ calcBody.addEventListener('click', function(event) {
 
   // clear valriables 
   if (elementId === "clear") {
-    firstNumberArray = [];
-    firstNumber = 0;
-    secondNumberArray = [];
-    secondNumber = 0;
-    operator = null;
+    clearVars();
     calcResult.textContent = 0;
-    console.log("cleared vars: " + firstNumber + secondNumber + operator);
   }
 
   // results output
   if (elementId === "equals") {
     if (operator === null) {
-    calcResult.textContent = firstNumber;
+    calcResult.textContent = Number(firstNumber);
     }
     else if (secondNumber === null) {
       secondNumber = 0;
+    } 
+    else {
+      result = Number(operate(firstNumber, secondNumber, operator));
+      if (result < 0 || result === undefined ) {
+        result = 0;
+      }
+      calcResult.textContent = result;
+      console.log(result);  
+      clearVars();
     }
-    result = operate(firstNumber, secondNumber, operator);
-    calcResult.textContent = result;
-    firstNumber = 0;
-    secondNumber = 0;
-    operator = null;
-    console.log(result);
   }
   console.log(elementText);
   console.log('Class: ' + elementClass);
